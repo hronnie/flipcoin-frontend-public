4d677f3e7f6a831d9483e8d1ed0ed619e2b9b58f
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2021-09-17 11:51:32 +0200

    Adds configuration for deploy

--- src/app/common/service/entry.service.ts
+++ src/app/common/service/entry.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Entry} from "../../model/entry/entry.model";
 import {Observable} from "rxjs";
+import {environment} from "../../../environments";
 
 @Injectable({
     providedIn: 'root'
@@ -12,6 +13,7 @@ export class EntryService {
     }
 
     getAllEntries(): Observable<Entry[]> {
-      return this.http.get<any>('http://localhost:5001/flipcoin-bot/us-central1/application/api/binance/frontendapi/entry/all');
+        const baseUrl = environment.apiUrl;
+        return this.http.get<any>(`${baseUrl}/binance/frontendapi/entry/all`);
     }
 }
---
+++ src/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://europe-west1-flipcoin-bot.cloudfunctions.net/application/api'
+};
