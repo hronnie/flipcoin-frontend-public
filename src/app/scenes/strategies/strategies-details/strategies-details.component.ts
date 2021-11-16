e77f54e5340166c3e04ffc1bdbb6d92f68443290
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2021-11-16 15:06:03 +0100

    Implements new copy methods

--- src/app/scenes/strategies/strategies-details/strategies-details.component.ts
+++ src/app/scenes/strategies/strategies-details/strategies-details.component.ts
@@ -3,6 +3,7 @@ import {StrategyService} from "../../../common/service/strategy.service";
 import {ActivatedRoute} from "@angular/router";
 import {Strategy} from "../../../model/entry/strategy.model";
 import {environment} from "../../../../environments/environment";
+import {Clipboard} from "@angular/cdk/clipboard";
 
 @Component({
     selector: 'app-strategies-details',
@@ -18,7 +19,8 @@ export class StrategiesDetailsComponent implements OnInit {
     exchange = "ftx";
 
     constructor(private strategyService: StrategyService,
-                private route: ActivatedRoute) {
+                private route: ActivatedRoute,
+                private clipboard: Clipboard) {
         this.route.params.subscribe(
             (params) => {
                 this.strategyId = params.strategyId;
@@ -30,6 +32,26 @@ export class StrategiesDetailsComponent implements OnInit {
         this.strategyService.getStrategy(this.strategyId).subscribe(strategyResult => {
             this.strategy = strategyResult;
         });
+    }
+
+    copyTextEntryExit(endpoint: string) {
+        const textToCopy = `${this.apiUrl}/${this.exchange}${endpoint}${(this.strategyId)}/${(this.symbol)}`;
+        this.clipboard.copy(textToCopy);
+    }
+
+    copyTextCondition(endpoint: string, index: any, condition: string) {
+        const textToCopy = `${this.apiUrl}/${this.exchange}${endpoint}${(this.strategyId)}/${(index)}/${condition}`;
+        this.clipboard.copy(textToCopy);
+    }
+
+    copyTextStatus(condition: string) {
+        const textToCopy = `${this.apiUrl}/${this.exchange}/tv/strategy/status/${(this.strategyId)}/${condition}`;
+        this.clipboard.copy(textToCopy);
+    }
+
+    copyTextType(type: string) {
+        const textToCopy = `${(this.apiUrl)}/${(this.exchange)}/tv/strategy/type/{{strategyId}}/${type}`;
+        this.clipboard.copy(textToCopy);
     }
 
 }
